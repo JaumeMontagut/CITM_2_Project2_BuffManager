07158ad113454608120b4436106341db72ed7cf0
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2019-03-15 11:48:50 +0100

    Testing implementation from this page
    https://gamedevelopment.tutsplus.com/tutorials/using-the-composite-design-pattern-for-an-rpg-attributes-system--gamedev-243

--- exercices/Buff_manager/j1Scene.h
+++ exercices/Buff_manager/j1Scene.h
@@ -6,6 +6,52 @@
 
 struct SDL_Texture;
 
+class BaseAttribute {
+private:
+	int base_value;
+	float base_multiplier;
+	
+public:
+	BaseAttribute(int value, float multiplier) :
+		base_value(value), base_multiplier(multiplier) {
+
+	}
+
+	int GetBaseValue() {
+		return base_value;
+	}
+
+	float GetMultiplier() {
+		return base_multiplier;
+	}
+};
+
+class RawBonus : public BaseAttribute {
+public:
+	RawBonus(int value = 0, float multiplier = 0.f) : BaseAttribute(value, multiplier) {
+
+	}
+};
+
+class FinalBonus : public BaseAttribute {
+public:
+	FinalBonus(int value = 0, float multiplier = 0.f) : BaseAttribute(value, multiplier) {
+
+	}
+};
+
+//class Attribute : public BaseAttribute {
+//private:
+//	RawBonus * raw_bonuses;
+//	FinalBonus * final_bonuses;
+//	int final_value;
+//
+//public:
+//	Attribute(int starting_value) : BaseAttribute(starting_value) {
+//
+//	}
+//};
+
 class j1Scene : public j1Module
 {
 public:
